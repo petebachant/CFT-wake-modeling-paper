0ce9a1a1f1a5cbb9aced153e384e0b4013019cf9
Pete Bachant <petebachant@gmail.com>
2015-03-19 19:44:53 -0400

    Modify getbib.py to download if necessary

--- getbib.py
+++ getbib.py
@@ -5,8 +5,19 @@ This script copies my personal BibTeX file to the current directory.
 
 import os
 import shutil
+import sys
+if sys.version_info[0] == 3:
+    from urllib.request import urlretrieve
+else:
+    from urllib import urlretrieve
+    FileNotFoundError = IOError
 
 bibpath = os.path.join(os.path.expanduser("~"), "Google Drive",
                        "Library", "Library.bib")
-
-shutil.copy2(bibpath, "library.bib")
+biburl = "https://raw.githubusercontent.com/petebachant/Library/master/Library.bib"
+                       
+try:
+    shutil.copy2(bibpath, "library.bib")
+except FileNotFoundError:
+    print("Downloading BibTeX file")
+    urlretrieve(biburl, "library.bib")
