8f1f6180551efa4d5567952fea19aeebb5e7068b
Pete Bachant <petebachant@gmail.com>
2015-07-28 14:32:29 -0400

    Add function for plotting mean u profiles

--- makefigs.py
+++ makefigs.py
@@ -6,16 +6,18 @@ This script makes the figures for the CFT wake modeling paper.
 from __future__ import print_function, division
 import matplotlib.pyplot as plt
 import os
+from pxl.styleplot import set_sns
 
 cfd_sst_dir = "/media/pete/Data1/OpenFOAM/pete-2.3.x/run/unh-rvat-3d/mesh14"
 cfd_sa_dir = "/media/pete/Data2/OpenFOAM/pete-2.3.x/run/unh-rvat-3d/mesh14-sa"
 cfd_sst_2d_dir = "/media/pete/Data1/OpenFOAM/pete-2.3.x/run/unh-rvat-2d/kOmegaSST"
 cfd_sa_2d_dir = "/media/pete/Data1/OpenFOAM/pete-2.3.x/run/unh-rvat-2d/SpalartAllmaras"
-cfd_alm_dir = ""
 exp_dir = "/home/pete/Google Drive/Research/Experiments/RVAT Re dep"
-paper_dir = os.getcwd()
-cfd_dirs = {"kOmegaSST": cfd_sst_dir, "SpalartAllmaras": cfd_sa_dir}
-cfd_dirs_2d = {"kOmegaSST": cfd_sst_2d_dir, "SpalartAllmaras": cfd_sa_2d_dir}
+paper_dir = "/home/pete/Google Drive/Research/Papers/CFT wake modeling"
+cfd_dirs = {"3-D": {"kOmegaSST": cfd_sst_dir, 
+                    "SpalartAllmaras": cfd_sa_dir},
+            "2-D": {"kOmegaSST": cfd_sst_2d_dir, 
+                    "SpalartAllmaras": cfd_sa_2d_dir}}
 
 save = True
 savetype = ".pdf"
@@ -38,21 +40,29 @@ def plot_exp_meancontquiv():
 
 def plot_cfd_meancontquiv(case="kOmegaSST"):
     """Plots wake mean velocity contours/quivers from 3-D CFD case."""
-    os.chdir(cfd_dirs[case])
+    os.chdir(cfd_dirs["3-D"][case])
     import processing as processing_cfd
     processing_cfd.plot_meancontquiv()
     os.chdir(paper_dir)
     if save:
          plt.savefig("figures/meancontquiv_" + case + savetype)
+         
+def plot_cfd_u_profile(case="kOmegaSST", dims="2-D"):
+    """Plot mean streamwise velocity profile."""
+    os.chdir(cfd_dirs[dims][case])
+    import modules.plotting as plotting_cfd
+    plotting_cfd.plot_u()
+    os.chdir(paper_dir)
     
-def main():
-    plot_exp_perf_curve()
-    plot_exp_meancontquiv()
-    plot_cfd_meancontquiv("kOmegaSST")
-    plot_cfd_meancontquiv("SpalartAllmaras")
-    plt.show()
     
 if __name__ == "__main__":
     if not os.path.isdir("figures"):
         os.mkdir("figures")
-    main()
+    set_sns()
+    
+#    plot_exp_perf_curve()
+#    plot_exp_meancontquiv()
+#    plot_cfd_meancontquiv("kOmegaSST")
+#    plot_cfd_meancontquiv("SpalartAllmaras")
+    plot_cfd_u_profile()
+    plt.show()
