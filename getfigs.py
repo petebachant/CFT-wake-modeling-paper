9b9e621aaffc2c0bd2fa4a4ee7b093ebcf94be23
Pete Bachant <petebachant@gmail.com>
2015-09-01 18:59:51 -0400

    Add docs to getfigs.py

--- getfigs.py
+++ getfigs.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 """
 This script copies figures to this directory.
+
+Figures that need to be copied:
+  * Mesh overview from 2-D SST RANS case
+  * Blade mesh closeup from 2-D SST RANS case
+  * Vorticity contours from 2-D SA case
 """
 
 import os
-
