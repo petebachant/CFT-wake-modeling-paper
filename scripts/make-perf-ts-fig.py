deee433801fd4faf5c76def7ad596255808b90ed
Pete Bachant <petebachant@gmail.com>
2016-10-02 13:08:21 -0400

    Plot revolution averaged C_P for clarity

--- scripts/make-perf-ts-fig.py
+++ scripts/make-perf-ts-fig.py
@@ -6,7 +6,7 @@ from pxl.styleplot import set_sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
-
+import numpy as np
 
 set_sns(context="paper")
 
@@ -14,13 +14,24 @@ fpath = "UNH-RVAT-3D-OpenFOAM/processed/perf.csv"
 
 df = pd.read_csv(fpath)
 
+# Create revolution-averaged data
+nrevs = int(df.theta_deg.max() / 360)
+rev = []
+cp_rev_ave = []
+for n in range(nrevs):
+    rev.append(n + 1)
+    cp_rev_ave.append(
+        df.cp[np.logical_and(df.theta_deg >= n*360,
+                             df.theta_deg < (n + 1)*360)].mean()
+    )
+
 fig, ax = plt.subplots()
 
-ax.plot(df.theta_deg, df.cp)
+ax.plot(rev, cp_rev_ave, marker="o")
 
-ax.set_xlabel(r"$\theta$ (degrees)")
+ax.set_xlabel(r"Rotor revolution")
 ax.set_ylabel("$C_P$")
-ax.set_ylim((0, 0.8))
+# ax.set_ylim((0, 0.8))
 
 fig.tight_layout()
 
