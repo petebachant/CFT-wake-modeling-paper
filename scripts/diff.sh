a97f34cd1264bf85a6252b76becfe715c4d15eec
Pete Bachant <petebachant@gmail.com>
2016-10-09 16:36:58 -0400

    Use diff script from VAT-ALM paper

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-# This script creates a diff version comparing the recent with whatever revision
-# is supplied as the first argument. This can be any valid Git identifier, e.g.,
-# a commit hash, tag, etc.
+# This script creates a diff version comparing the current version with the
+# most recent tagged version
 
+PREVIOUS=$(git tag | tail -n1)
 CURRENT=$(git describe --always --dirty)
-rev=$1
-if [ "$rev" = "" ]; then
-    rev=$(git tag | tail -n1)
+
+# If current version is a tag, use one tag prior
+if [ $CURRENT = $PREVIOUS ]
+then
+    PREVIOUS=$(git tag | tail -n2 | head -n1)
 fi
-git show $rev:paper.tex > archive/paper-$rev-$CURRENT.tex
-latexdiff --exclude-textcmd="section,subsection,subsubsection" --math-markup=off archive/paper-$rev-$CURRENT.tex paper.tex > paper-$rev-$CURRENT-diff.tex
-latexmk paper-$rev-$CURRENT-diff.tex -pdf -f
-mv paper-$rev-$CURRENT-diff.pdf archive
-rm paper-$rev-$CURRENT*
+
+git show $PREVIOUS:paper.tex > archive/paper-$PREVIOUS.tex
+latexdiff --exclude-textcmd="section,subsection,subsubsection" --math-markup=off archive/paper-$PREVIOUS.tex paper.tex > paper-$PREVIOUS-$CURRENT-diff.tex
+latexmk paper-$PREVIOUS-$CURRENT-diff.tex -pdf -f
+mv paper-$PREVIOUS-$CURRENT-diff.pdf archive
+rm paper-$PREVIOUS-$CURRENT*
